bfba71316639095d27aff674802e15083f966358
okolya <okolya@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-27 23:38:20 +0000

    First Uploading
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ws_wordpressgrab@37347 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ trunk/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ws_wordpressgrab".
+#
+# Auto generated 27-08-2010 21:47
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Wordpress Grabber',
+	'description' => 'Get post from wordpress blog',
+	'category' => 'plugin',
+	'author' => 'Nikolay Orlenko',
+	'author_email' => 'okolya@gmail.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'Web.Spectr http://web-spectr.com',
+	'version' => '0.2.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"7e7f";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"98c3";s:17:"ext_localconf.php";s:4:"e37d";s:14:"ext_tables.php";s:4:"2981";s:14:"ext_tables.sql";s:4:"e1f8";s:15:"flexform_ds.xml";s:4:"5a61";s:27:"icon_tx_wswordpressgrab.gif";s:4:"98c3";s:13:"locallang.xml";s:4:"810a";s:16:"locallang_db.xml";s:4:"e348";s:7:"tca.php";s:4:"361e";s:36:"pi1/class.tx_wswordpressgrab_pi1.php";s:4:"42c4";s:17:"pi1/locallang.xml";s:4:"7dba";s:30:"res/jquery/jquery-1.4.2.min.js";s:4:"1009";}',
+	'suggests' => array(
+	),
+);
+
+?>
