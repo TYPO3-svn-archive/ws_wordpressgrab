85ea5ec648f3dec60ebac138e78ccf3ba0492e02
okolya <okolya@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-21 21:02:01 +0000

    add hide checkbox for description and addthis service
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ws_wordpressgrab/trunk@51281 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ws_wordpressgrab".
 #
-# Auto generated 27-08-2010 21:47
+# Auto generated 11-08-2011 23:39
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -14,22 +14,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Wordpress Grabber',
 	'description' => 'Get post from wordpress blog',
 	'category' => 'plugin',
-	'author' => 'Nikolay Orlenko',
-	'author_email' => 'okolya@gmail.com',
-	'shy' => '',
+	'shy' => 0,
+	'version' => '0.4.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'beta',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
+	'author' => 'Nikolay Orlenko',
+	'author_email' => 'okolya@gmail.com',
 	'author_company' => 'Web.Spectr http://web-spectr.com',
-	'version' => '0.2.0',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,9 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"7e7f";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"98c3";s:17:"ext_localconf.php";s:4:"e37d";s:14:"ext_tables.php";s:4:"2981";s:14:"ext_tables.sql";s:4:"e1f8";s:15:"flexform_ds.xml";s:4:"5a61";s:27:"icon_tx_wswordpressgrab.gif";s:4:"98c3";s:13:"locallang.xml";s:4:"810a";s:16:"locallang_db.xml";s:4:"e348";s:7:"tca.php";s:4:"361e";s:36:"pi1/class.tx_wswordpressgrab_pi1.php";s:4:"42c4";s:17:"pi1/locallang.xml";s:4:"7dba";s:30:"res/jquery/jquery-1.4.2.min.js";s:4:"1009";}',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"7e7f";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"98c3";s:17:"ext_localconf.php";s:4:"e37d";s:14:"ext_tables.php";s:4:"2981";s:14:"ext_tables.sql";s:4:"e1f8";s:15:"flexform_ds.xml";s:4:"20f2";s:27:"icon_tx_wswordpressgrab.gif";s:4:"98c3";s:13:"locallang.xml";s:4:"2138";s:16:"locallang_db.xml";s:4:"e348";s:7:"tca.php";s:4:"361e";s:14:"doc/manual.sxw";s:4:"c7c9";s:36:"pi1/class.tx_wswordpressgrab_pi1.php";s:4:"b80b";s:17:"pi1/locallang.xml";s:4:"7dba";}',
 );
 
 ?>
